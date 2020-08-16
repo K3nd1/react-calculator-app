4c9b2409f541537a7ed2b5587099669dbe9dbaed
Okendi Gjepali <42900883+K3nd1@users.noreply.github.com>
2020-08-16 12:57:53 -0700

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,11 +7,10 @@ const isOperator = val => {
 
 export const Button = props => (
   <div
-    className={`button-wrapper ${
-      isOperator(props.children) ? null : "operator"
-    }`}
-    onClick={() => props.handleClick(props.children)}
-  >
+    
+    className={`button-wrapper ${  isOperator(props.children) ? null : "operator" }`}
+    onClick={() => props.handleClick(props.children)}>
     {props.children}
+    
   </div>
 );
